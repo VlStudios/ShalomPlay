76418082240cb1ad4662c361612c45c804c47515
lar <larinho2020@gmail.com>
2022-06-13 23:34:24 -0300

    alteração 02

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ const App = () => {
     const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
 
     useEffect(() => {
-      AsyncStorage.getItem('alreadyLaunched').then.useState(value => {
-        if(value = null) {
+      AsyncStorage.getItem('alreadyLaunched').then(value => {
+        if(value == null) {
           AsyncStorage.setItem('alreadyLaunched' , 'true');
           setIsFirstLaunch(true);
         }else {
